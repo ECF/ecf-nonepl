76d65008f634f6bfc417616ae2d309e1b71da67c
slewis <slewis>
2005-10-29 01:00:19 +0000

    Updates to rcpchat copyrights notices to include Ed Burnette

--- applications/org.eclipse.ecf.example.rcpchat/src/org/eclipse/ecf/example/rcpchat/wizard/ConnectWizard.java
+++ applications/org.eclipse.ecf.example.rcpchat/src/org/eclipse/ecf/example/rcpchat/wizard/ConnectWizard.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2004 Composent, Inc. and others. All rights reserved. This
+ * Copyright (c) 2005 Ed Burnette, Composent, Inc. and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: Composent, Inc. - initial API and implementation
+ * Contributors: Ed Burnette, Composent, Inc. - initial API and implementation
  ******************************************************************************/
 package org.eclipse.ecf.example.rcpchat.wizard;
 
