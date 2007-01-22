cfc663c44f6b8f829e597e7abb51f87994e27cf3
slewis <slewis>
2007-01-22 07:06:54 +0000

    Checked in v2.2.1 of smack api.  Also changed IRosterItem.getParent() to return IRosterItem rather than Object.  Changed refernences in constructors of IRosterEntry, IRosterGroup to refere to IRosterItem, also had Roster extend RosterItem.

--- plugins/org.eclipse.ecf.provider.yahoo/src/org/eclipse/ecf/provider/yahoo/container/YahooPresenceContainer.java
+++ plugins/org.eclipse.ecf.provider.yahoo/src/org/eclipse/ecf/provider/yahoo/container/YahooPresenceContainer.java
@@ -239,7 +239,7 @@ public class YahooPresenceContainer extends AbstractPresenceContainer {
 	/**
 	 * @return
 	 */
-	protected Object getRoster() {
+	protected IRoster getRoster() {
 		return roster;
 	}
 
