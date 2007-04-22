c3ca3dd484d8d1e4364ab7276063ee8a17dcefaf
slewis <slewis>
2007-04-22 23:01:21 +0000

    Fix for yahoo connect wizard after UI change

--- plugins/org.eclipse.ecf.provider.yahoo.ui/src/org/eclipse/ecf/internal/provider/yahoo/ui/wizards/YahooConnectWizard.java
+++ plugins/org.eclipse.ecf.provider.yahoo.ui/src/org/eclipse/ecf/internal/provider/yahoo/ui/wizards/YahooConnectWizard.java
@@ -86,7 +86,7 @@ public final class YahooConnectWizard extends Wizard implements IConnectWizard {
 							.getSite().getAdapter(
 									IWorkbenchSiteProgressService.class);
 					view.openTab(icms, itms, targetID, message.getFromID());
-					view.showMessage(message.getFromID(), message.getBody());
+					view.showMessage(message);
 					service.warnOfContentChange();
 				} else {
 					try {
@@ -105,8 +105,7 @@ public final class YahooConnectWizard extends Wizard implements IConnectWizard {
 						}
 						view.openTab(icms, itms, targetID, message.getFromID());
 						view
-								.showMessage(message.getFromID(), message
-										.getBody());
+								.showMessage(message);
 					} catch (PartInitException e) {
 						e.printStackTrace();
 					}
