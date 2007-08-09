de1020ed952b138cc6fdbb286ecac797e0cee1c7
slewis <slewis>
2007-08-09 00:09:12 +0000

    Initial checkin of JMS provider tests.

---
+++ tests/org.eclipse.ecf.tests.provider.jms/src/org/eclipse/ecf/tests/provider/jms/JMSContainerTestCase.java
@@ -0,0 +1,33 @@
+package org.eclipse.ecf.tests.provider.jms;
+
+import org.eclipse.ecf.core.IContainer;
+import org.eclipse.ecf.core.identity.ID;
+import org.eclipse.ecf.core.identity.IDFactory;
+import org.eclipse.ecf.tests.ContainerAbstractTestCase;
+
+public class JMSContainerTestCase extends ContainerAbstractTestCase {
+
+	@Override
+	protected String getClientContainerName() {
+		return "ecf.jms.weblogic.client";
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		clients = createClients();
+		super.setUp();
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		cleanUpClients();
+		super.tearDown();
+	}
+	
+	public void testConnectClient() throws Exception {
+		IContainer client = getClients()[0];
+		ID targetID = IDFactory.getDefault().createID(client.getConnectNamespace(),new Object [] { "t3://localhost:7001/wlevs.airlineDemo.eventsFromSOA.topic"});
+		client.connect(targetID, null);
+		Thread.sleep(3000);
+	}
+}
