3c34c61cf02791a9cc9381e050a6ac691120978c
slewis <slewis>
2007-08-30 04:40:13 +0000

    Removed override annotation

--- tests/org.eclipse.ecf.tests.provider.jms/src/org/eclipse/ecf/tests/provider/jms/JMSContainerAbstractTestCase.java
+++ tests/org.eclipse.ecf.tests.provider.jms/src/org/eclipse/ecf/tests/provider/jms/JMSContainerAbstractTestCase.java
@@ -7,33 +7,30 @@ import org.eclipse.ecf.tests.ContainerAbstractTestCase;
 
 public abstract class JMSContainerAbstractTestCase extends ContainerAbstractTestCase {
 
-	@Override
 	protected abstract String getClientContainerName();
-	
+
 	protected abstract String getServerIdentity();
-	
+
 	protected abstract String getServerContainerName();
 
 	protected String getJMSNamespace() {
 		return "ecf.namespace.jmsid";
 	}
-	
-	@Override
+
 	protected void setUp() throws Exception {
 		setClientCount(1);
 		createServerAndClients();
 		super.setUp();
 	}
-	
-	@Override
+
 	protected void tearDown() throws Exception {
 		cleanUpServerAndClients();
 		super.tearDown();
 	}
-	
+
 	public void testConnectClient() throws Exception {
-		IContainer client = getClients()[0];
-		ID targetID = IDFactory.getDefault().createID(client.getConnectNamespace(),new Object [] {  getServerIdentity() });
+		final IContainer client = getClients()[0];
+		final ID targetID = IDFactory.getDefault().createID(client.getConnectNamespace(), new Object[] {getServerIdentity()});
 		client.connect(targetID, null);
 		Thread.sleep(3000);
 	}
