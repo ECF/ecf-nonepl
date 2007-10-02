904226dbea9e3abcc1f4bff96ed9e63f2a023e15
ted <ted>
2007-10-02 21:19:17 +0000

    *** empty log message ***

---
+++ www/dailies.php
@@ -0,0 +1,19 @@
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html>
+<head>
+<title>eclipse communication framework dailies</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" href="default_style.css" type="text/css">
+</head>
+<body text="#000000" bgcolor="#ffffff" link="#0000ee" vlink="#551a8b" alink="#ff0000"><table width=100% BORDER=0 CELLPADDING=2 CELLSPACING=5 STYLE="page-break-before: always">
+<h1>Under Construction</h1>
+<div>
+<p>
+</p>
+<?php
+
+
+?>
+</div>
+</body>
+</html>
