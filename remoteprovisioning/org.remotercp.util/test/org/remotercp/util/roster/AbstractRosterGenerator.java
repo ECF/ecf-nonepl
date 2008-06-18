6dd5b5035a917bcc73a7cc3a7eaffb87a14da6ea
reiswich <reiswich>
2008-06-18 19:10:35 +0000

    abstract test for rosterentries added

---
+++ remoteprovisioning/org.remotercp.util/test/org/remotercp/util/roster/AbstractRosterGenerator.java
@@ -0,0 +1,102 @@
+package org.remotercp.util.roster;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.fail;
+
+import org.eclipse.ecf.core.identity.BaseID;
+import org.eclipse.ecf.core.identity.ID;
+import org.eclipse.ecf.core.identity.IDCreateException;
+import org.eclipse.ecf.core.identity.Namespace;
+import org.eclipse.ecf.core.user.IUser;
+import org.eclipse.ecf.core.user.User;
+import org.eclipse.ecf.presence.IPresence;
+import org.eclipse.ecf.presence.Presence;
+import org.eclipse.ecf.presence.roster.IRosterEntry;
+import org.eclipse.ecf.presence.roster.IRosterGroup;
+import org.eclipse.ecf.presence.roster.RosterEntry;
+
+public class AbstractRosterGenerator {
+
+	public IRosterEntry createRosterEntry(String rosterName, IRosterGroup group,
+			IPresence.Type presencetype) {
+		TestNameSpace namespace = new TestNameSpace();
+		ID userID = null;
+
+		try {
+			userID = namespace.createInstance(new String[] { rosterName });
+		} catch (IDCreateException e) {
+			e.printStackTrace();
+			fail();
+		}
+
+		assertNotNull(userID);
+
+		IUser user = new User(userID, rosterName);
+
+		IPresence presence = new Presence(presencetype);
+
+		RosterEntry rosterEntry = new RosterEntry(group, user, presence);
+
+		return rosterEntry;
+	}
+
+	private class TestID extends BaseID {
+
+		private static final long serialVersionUID = 1L;
+
+		private String username;
+
+		public TestID(TestNameSpace namespace, String username) {
+			super(namespace);
+			this.username = username;
+		}
+
+		@Override
+		protected int namespaceCompareTo(BaseID o) {
+			return 0;
+		}
+
+		@Override
+		protected boolean namespaceEquals(BaseID o) {
+			return o.getName().equals(username);
+		}
+
+		@Override
+		protected String namespaceGetName() {
+			return username;
+		}
+
+		@Override
+		protected int namespaceHashCode() {
+			return username.hashCode();
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o instanceof TestID) {
+				TestID id = (TestID) o;
+				return username.equals(id.username);
+			} else {
+				fail();
+				return false;
+			}
+		}
+	}
+
+	private class TestNameSpace extends Namespace {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public ID createInstance(Object[] parameters) throws IDCreateException {
+			String username = (String) parameters[0];
+			return new TestID(this, username);
+		}
+
+		@Override
+		public String getScheme() {
+			return null;
+		}
+
+	}
+}
--- remoteprovisioning/org.remotercp.util/test/org/remotercp/util/roster/TestRosterUtil.java
+++ remoteprovisioning/org.remotercp.util/test/org/remotercp/util/roster/TestRosterUtil.java
@@ -3,30 +3,22 @@ package org.remotercp.util.roster;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.eclipse.ecf.core.identity.BaseID;
 import org.eclipse.ecf.core.identity.ID;
-import org.eclipse.ecf.core.identity.IDCreateException;
-import org.eclipse.ecf.core.identity.Namespace;
-import org.eclipse.ecf.core.user.IUser;
-import org.eclipse.ecf.core.user.User;
 import org.eclipse.ecf.presence.IPresence;
-import org.eclipse.ecf.presence.Presence;
 import org.eclipse.ecf.presence.roster.IRoster;
 import org.eclipse.ecf.presence.roster.IRosterEntry;
 import org.eclipse.ecf.presence.roster.IRosterGroup;
 import org.eclipse.ecf.presence.roster.Roster;
-import org.eclipse.ecf.presence.roster.RosterEntry;
 import org.eclipse.ecf.presence.roster.RosterGroup;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestRosterUtil {
+public class TestRosterUtil extends AbstractRosterGenerator {
 
 	private IRoster roster;
 
@@ -40,13 +32,13 @@ public class TestRosterUtil {
 	public void testUserPresenceInContextMenu() {
 
 		List<IRosterEntry> rosterEntries = RosterUtil.getRosterEntries(roster);
-		assertEquals(4, rosterEntries.size());
+		assertEquals(7, rosterEntries.size());
 
 		List<IRosterEntry> group1Entries = RosterUtil.getRosterEntries(group1);
 		assertEquals(2, group1Entries.size());
 
 		List<IRosterEntry> group2Entries = RosterUtil.getRosterEntries(group2);
-		assertEquals(3, group2Entries.size());
+		assertEquals(5, group2Entries.size());
 	}
 
 	@Test
@@ -62,31 +54,6 @@ public class TestRosterUtil {
 	@SuppressWarnings("unchecked")
 	@Before
 	public void initRoster() {
-		TestNameSpace namespace = new TestNameSpace();
-		ID user1ID = null;
-		ID user2ID = null;
-		ID user3ID = null;
-		ID user4ID = null;
-
-		try {
-			user1ID = namespace.createInstance(new String[] { "klaus" });
-			user2ID = namespace.createInstance(new String[] { "susi" });
-			user3ID = namespace.createInstance(new String[] { "marie" });
-			user4ID = namespace.createInstance(new String[] { "peter" });
-		} catch (IDCreateException e) {
-			e.printStackTrace();
-			fail();
-		}
-
-		assertNotNull(user1ID);
-		assertNotNull(user2ID);
-		assertNotNull(user3ID);
-		assertNotNull(user4ID);
-
-		IUser user1 = new User(user1ID, "Klaus");
-		IUser user2 = new User(user2ID, "Susi");
-		IUser user3 = new User(user3ID, "Marie");
-		IUser user4 = new User(user4ID, "Peter");
 
 		roster = new Roster(null);
 
@@ -96,18 +63,22 @@ public class TestRosterUtil {
 		roster.getItems().add(group1);
 		roster.getItems().add(group2);
 
-		IPresence presence1 = new Presence(IPresence.Type.AVAILABLE);
-		IPresence presence2 = new Presence(IPresence.Type.UNAVAILABLE);
+		IRosterEntry rosterEntry1 = super.createRosterEntry("Klaus", group1,
+				IPresence.Type.AVAILABLE);
+		IRosterEntry rosterEntry2 = super.createRosterEntry("Susi", group1,
+				IPresence.Type.UNAVAILABLE);
 
-		RosterEntry rosterEntry1 = new RosterEntry(group1, user1, presence1);
-		RosterEntry rosterEntry2 = new RosterEntry(group1, user2, presence2);
+		IRosterEntry rosterEntry3 = super.createRosterEntry("Peter", group2,
+				IPresence.Type.UNAVAILABLE);
+		IRosterEntry rosterEntry4 = super.createRosterEntry("Sandra", group2,
+				IPresence.Type.UNAVAILABLE);
+		IRosterEntry rosterEntry5 = super.createRosterEntry("Jack", group2,
+				IPresence.Type.UNAVAILABLE);
+		IRosterEntry rosterEntry6 = super.createRosterEntry("Mary", group2,
+				IPresence.Type.UNAVAILABLE);
 
-		RosterEntry rosterEntry3 = new RosterEntry(group2, user2, presence2);
-		RosterEntry rosterEntry4 = new RosterEntry(group2, user3, presence2);
-		RosterEntry rosterEntry5 = new RosterEntry(group2, user4, presence2);
-		RosterEntry rosterEntry6 = new RosterEntry(group2, user4, presence2);
-
-		RosterEntry rosterEntry7 = new RosterEntry(roster, user4, presence1);
+		IRosterEntry rosterEntry7 = super.createRosterEntry("Susan", group2,
+				IPresence.Type.AVAILABLE);
 
 		this.rosterEntries = new ArrayList<IRosterEntry>();
 		rosterEntries.add(rosterEntry1);
@@ -128,65 +99,5 @@ public class TestRosterUtil {
 			assertNotNull(group.getEntries());
 			assertFalse(group.getEntries().isEmpty());
 		}
-	}
-
-	private class TestID extends BaseID {
-
-		private static final long serialVersionUID = 1L;
-
-		private String username;
-
-		public TestID(TestNameSpace namespace, String username) {
-			super(namespace);
-			this.username = username;
-		}
-
-		@Override
-		protected int namespaceCompareTo(BaseID o) {
-			return 0;
-		}
-
-		@Override
-		protected boolean namespaceEquals(BaseID o) {
-			return o.getName().equals(username);
-		}
-
-		@Override
-		protected String namespaceGetName() {
-			return username;
-		}
-
-		@Override
-		protected int namespaceHashCode() {
-			return username.hashCode();
-		}
-
-		@Override
-		public boolean equals(Object o) {
-			if (o instanceof TestID) {
-				TestID id = (TestID) o;
-				return username.equals(id.username);
-			} else {
-				fail();
-				return false;
-			}
-		}
-	}
-
-	private class TestNameSpace extends Namespace {
-
-		private static final long serialVersionUID = 1L;
-
-		@Override
-		public ID createInstance(Object[] parameters) throws IDCreateException {
-			String username = (String) parameters[0];
-			return new TestID(this, username);
-		}
-
-		@Override
-		public String getScheme() {
-			return null;
-		}
-
 	}
 }
