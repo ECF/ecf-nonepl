fe121d2ba07a3a034b717a39f23e61b114cd0e71
reiswich <reiswich>
2008-06-24 12:51:48 +0000

    connect and disconnect toobar actions added

---
+++ remoteprovisioning/org.remotercp.ecf/src/org/remotercp/ecf/images/ImageKeys.java
@@ -0,0 +1,13 @@
+package org.remotercp.ecf.images;
+
+public class ImageKeys {
+
+	private ImageKeys() {
+		// nothing to do
+	}
+
+	public static final String CONNECT = "icons/network_connect.png";
+
+	public static final String DISCONNECT = "icons/network_disconnect.png";
+
+}
