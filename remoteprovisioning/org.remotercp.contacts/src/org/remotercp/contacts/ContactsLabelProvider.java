fe6409c8ab9466e736c9e327d669224ce0e976cc
reiswich <reiswich>
2008-08-03 14:57:55 +0000

    copyright deleted

--- remoteprovisioning/org.remotercp.contacts/src/org/remotercp/contacts/ContactsLabelProvider.java
+++ remoteprovisioning/org.remotercp.contacts/src/org/remotercp/contacts/ContactsLabelProvider.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2004, 2005 Jean-Michel Lemieux, Jeff McAffer and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Hyperbola is an RCP application developed for the book 
- *     Eclipse Rich Client Platform - 
- *         Designing, Coding, and Packaging Java Applications 
- * See http://eclipsercp.org
- * 
- * Contributors:
- *     Jean-Michel Lemieux and Jeff McAffer - initial implementation
- *******************************************************************************/
 package org.remotercp.contacts;
 
 import java.util.HashMap;
