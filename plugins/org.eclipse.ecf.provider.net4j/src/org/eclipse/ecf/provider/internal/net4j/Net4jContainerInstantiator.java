cc2aab3bbf35086d9fe0f05150bf4d6ae89b16af
slewis <slewis>
2008-11-11 23:16:52 +0000

    See discussion on bug 203406.  Removed direct dependencies on Net4j provider from test code.  Also simplified test code and added functionality to Net4jContainerInstantiator to do appropriate net4j initialization

--- plugins/org.eclipse.ecf.provider.net4j/src/org/eclipse/ecf/provider/internal/net4j/Net4jContainerInstantiator.java
+++ plugins/org.eclipse.ecf.provider.net4j/src/org/eclipse/ecf/provider/internal/net4j/Net4jContainerInstantiator.java
@@ -17,43 +17,59 @@ import org.eclipse.ecf.core.identity.ID;
 import org.eclipse.ecf.core.identity.IDCreateException;
 import org.eclipse.ecf.core.identity.IDFactory;
 import org.eclipse.ecf.core.provider.BaseContainerInstantiator;
+import org.eclipse.ecf.core.security.IConnectContext;
+import org.eclipse.ecf.provider.net4j.INet4jConnectorProvider;
+import org.eclipse.net4j.Net4jUtil;
+import org.eclipse.net4j.connector.IConnector;
+import org.eclipse.net4j.tcp.TCPUtil;
+import org.eclipse.net4j.util.container.ManagedContainer;
 
 /**
  * @author Eike Stepper
  */
-public class Net4jContainerInstantiator extends BaseContainerInstantiator
-{
-  @Override
-  public IContainer createInstance(ContainerTypeDescription description, Object[] parameters)
-      throws ContainerCreateException
-  {
-    try
-    {
-      if (parameters != null && parameters.length > 0)
-      {
-        if (parameters[0] instanceof ID)
-        {
-          return new Net4jContainer((ID)parameters[0]);
-        }
+public class Net4jContainerInstantiator extends BaseContainerInstantiator {
+	@Override
+	public IContainer createInstance(ContainerTypeDescription description,
+			Object[] parameters) throws ContainerCreateException {
+		try {
+			Net4jContainer container = null;
 
-        if (parameters[0] instanceof String)
-        {
-          return new Net4jContainer(IDFactory.getDefault().createStringID((String)parameters[0]));
-        }
-      }
+			if (parameters != null && parameters.length > 0) {
+				if (parameters[0] instanceof ID) {
+					container = new Net4jContainer((ID) parameters[0]);
+				}
 
-      return new Net4jContainer();
-    }
-    catch (IDCreateException ex)
-    {
-      throw new ContainerCreateException("Exception creating ID for net4j container", ex);
-    }
-  }
+				if (parameters[0] instanceof String) {
+					container = new Net4jContainer(IDFactory.getDefault()
+							.createStringID((String) parameters[0]));
+				}
+			}
 
-  @Override
-  public String[] getSupportedAdapterTypes(ContainerTypeDescription description)
-  {
-    // TODO Return String [] with adapter types supported for the given description
-    return super.getSupportedAdapterTypes(description);
-  }
+			if (container == null) {
+				container = new Net4jContainer();
+			}
+			// Now setup
+			container.setConnectorProvider(new INet4jConnectorProvider() {
+				public IConnector getConnector(ID targetID,
+						IConnectContext connectContext) {
+					ManagedContainer container = new ManagedContainer();
+					Net4jUtil.prepareContainer(container);
+					TCPUtil.prepareContainer(container);
+					return TCPUtil.getConnector(container, "localhost");
+				}
+			});
+
+			return container;
+
+		} catch (IDCreateException ex) {
+			throw new ContainerCreateException(
+					"Exception creating ID for net4j container", ex);
+		}
+	}
+
+	@Override
+	public String[] getSupportedAdapterTypes(
+			ContainerTypeDescription description) {
+		return super.getSupportedAdapterTypes(description);
+	}
 }
--- tests/org.eclipse.ecf.tests.provider.net4j/src/org/eclipse/ecf/tests/provider/net4j/ContainerCreateTests.java
+++ tests/org.eclipse.ecf.tests.provider.net4j/src/org/eclipse/ecf/tests/provider/net4j/ContainerCreateTests.java
@@ -10,81 +10,63 @@
  **************************************************************************/
 package org.eclipse.ecf.tests.provider.net4j;
 
-import org.eclipse.net4j.Net4jUtil;
-import org.eclipse.net4j.connector.IConnector;
-import org.eclipse.net4j.tcp.TCPUtil;
-import org.eclipse.net4j.util.container.ManagedContainer;
+import junit.framework.TestCase;
 
+import org.eclipse.ecf.core.ContainerFactory;
 import org.eclipse.ecf.core.IContainer;
 import org.eclipse.ecf.core.identity.ID;
+import org.eclipse.ecf.core.identity.IDFactory;
 import org.eclipse.ecf.core.identity.StringID;
-import org.eclipse.ecf.core.security.IConnectContext;
 import org.eclipse.ecf.datashare.IChannel;
 import org.eclipse.ecf.datashare.IChannelContainerAdapter;
 import org.eclipse.ecf.datashare.IChannelListener;
 import org.eclipse.ecf.datashare.events.IChannelEvent;
 import org.eclipse.ecf.datashare.events.IChannelMessageEvent;
-import org.eclipse.ecf.provider.internal.net4j.Net4jContainer;
 import org.eclipse.ecf.provider.internal.net4j.datashare.Net4jChannelContainerAdapter;
-import org.eclipse.ecf.provider.net4j.INet4jConnectorProvider;
-import org.eclipse.ecf.provider.net4j.Net4jContainerNamespace;
-
-import junit.framework.TestCase;
 
 /**
  * @author Eike Stepper
  */
-public class ContainerCreateTests extends TestCase
-{
-  public void testCreateContainer() throws Exception
-  {
-    IContainer container1 = createContainer();
-    IChannel channel = createChannel(container1, 1);
+public class ContainerCreateTests extends TestCase {
 
-    IContainer container2 = createContainer();
-    createChannel(container2, 1);
+	public void testCreateContainer() throws Exception {
+		IContainer container1 = createAndConnectContainer();
+		IChannel channel = createChannel(container1, 1);
 
-    channel.sendMessage("Eike Stepper".getBytes());
-    Thread.sleep(200);
-  }
+		IContainer container2 = createAndConnectContainer();
+		createChannel(container2, 1);
 
-  private IContainer createContainer() throws Exception
-  {
-    Net4jContainer container = new Net4jContainer();
-    container.setConnectorProvider(new INet4jConnectorProvider()
-    {
-      public IConnector getConnector(ID targetID, IConnectContext connectContext)
-      {
-        ManagedContainer container = new ManagedContainer();
-        Net4jUtil.prepareContainer(container);
-        TCPUtil.prepareContainer(container);
-        return TCPUtil.getConnector(container, "localhost");
-      }
-    });
+		channel.sendMessage("Eike Stepper".getBytes());
+		Thread.sleep(200);
+	}
 
-    ID targetID = new Net4jContainerNamespace().createInstance(new String[] { "tcp:localhost" });
-    container.connect(targetID, null);
-    return container;
-  }
+	private IContainer createAndConnectContainer() throws Exception {
+		IContainer container = ContainerFactory.getDefault().createContainer(
+				OM.CLIENT_CONTAINER_FACTORY);
 
-  private IChannel createChannel(IContainer container, int i) throws Exception
-  {
-    IChannelContainerAdapter adapter = (IChannelContainerAdapter)container.getAdapter(IChannelContainerAdapter.class);
-    assertTrue(adapter instanceof Net4jChannelContainerAdapter);
+		ID targetID = IDFactory.getDefault().createID(
+				container.getConnectNamespace(), "tcp:localhost");
+		container.connect(targetID, null);
+		return container;
+	}
 
-    ID channelID = new StringID.StringIDNamespace().createInstance(new String[] { "CHANNEL-" + i });
-    return adapter.createChannel(channelID, new IChannelListener()
-    {
-      public void handleChannelEvent(IChannelEvent event)
-      {
-        if (event instanceof IChannelMessageEvent)
-        {
-          IChannelMessageEvent e = (IChannelMessageEvent)event;
-          System.out.println("Sender:  " + e.getFromContainerID());
-          System.out.println("Channel: " + e.getChannelID());
-          System.out.println("Message: " + new String(e.getData()));
-        }
-      }
-    }, null);
-  }
+	private IChannel createChannel(IContainer container, int i)
+			throws Exception {
+		IChannelContainerAdapter adapter = (IChannelContainerAdapter) container
+				.getAdapter(IChannelContainerAdapter.class);
+		assertTrue(adapter instanceof Net4jChannelContainerAdapter);
+
+		ID channelID = new StringID.StringIDNamespace()
+				.createInstance(new String[] { "CHANNEL-" + i });
+		return adapter.createChannel(channelID, new IChannelListener() {
+			public void handleChannelEvent(IChannelEvent event) {
+				if (event instanceof IChannelMessageEvent) {
+					IChannelMessageEvent e = (IChannelMessageEvent) event;
+					System.out.println("Sender:  " + e.getFromContainerID());
+					System.out.println("Channel: " + e.getChannelID());
+					System.out.println("Message: " + new String(e.getData()));
+				}
+			}
+		}, null);
+	}
 }
