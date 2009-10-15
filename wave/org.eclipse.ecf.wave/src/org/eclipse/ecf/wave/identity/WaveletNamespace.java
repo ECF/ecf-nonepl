4f4c1663f491f90f8f2340a3493381669f8e39c8
slewis <slewis>
2009-10-15 23:46:21 +0000

    Added waveletid/namespace

--- wave/org.eclipse.ecf.wave/src/org/eclipse/ecf/wave/identity/WaveletNamespace.java
+++ wave/org.eclipse.ecf.wave/src/org/eclipse/ecf/wave/identity/WaveletNamespace.java
@@ -5,14 +5,14 @@ import org.eclipse.ecf.core.identity.IDCreateException;
 import org.eclipse.ecf.core.identity.Namespace;
 import org.waveprotocol.wave.model.id.LongIdSerialiser;
 
-public class WaveNamespace extends Namespace {
+public class WaveletNamespace extends Namespace {
 
-	public static final String NAME = "ecf.googlewave.wave.ns";
-	public static final String SCHEME = "wave";
+	public static final String NAME = "ecf.googlewave.wavelet.ns";
+	public static final String SCHEME = "wavelet";
 	
 	private static final long serialVersionUID = 2615028840514406159L;
 
-	public WaveNamespace() {
+	public WaveletNamespace() {
 		super(NAME,null);
 	}
 	
@@ -36,15 +36,15 @@ public class WaveNamespace extends Namespace {
 		try {
 			String init = getInitFromExternalForm(parameters);
 			if (init != null)
-				return new WaveID(this, LongIdSerialiser.INSTANCE.deserialiseWaveId(init));
+				return new WaveletID(this, LongIdSerialiser.INSTANCE.deserialiseWaveletId(init));
 			if (parameters.length == 1) {
 				if (parameters[0] instanceof String) {
-					return new WaveID(this, LongIdSerialiser.INSTANCE.deserialiseWaveId((String) parameters[0]));
+					return new WaveletID(this, LongIdSerialiser.INSTANCE.deserialiseWaveletId((String) parameters[0]));
 				}
 			}
-			throw new IllegalArgumentException("Invalid WaveId creation arguments");
+			throw new IllegalArgumentException("Invalid WaveletID creation arguments");
 		} catch (Exception e) {
-			throw new IDCreateException("WaveID creation failed", e);
+			throw new IDCreateException("WaveletID creation failed", e);
 		}
 	}
 
