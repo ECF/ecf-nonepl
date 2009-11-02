371340273400c0284cf923a1c989e2256a056862
wim.jongman <wim.jongman>
2009-11-02 22:20:21 +0000

    Test Commit

---
+++ newsreader/bundles/org.eclipse.ecf.samples.nntp/src/org/eclipse/ecf/samples/nntp/Snippet001.java
@@ -0,0 +1,13 @@
+package org.eclipse.ecf.samples.nntp;
+
+public class Snippet001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
