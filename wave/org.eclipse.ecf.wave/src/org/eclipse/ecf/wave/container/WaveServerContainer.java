937a5d2da381224e59ebf72925df42d87f10749c
slewis <slewis>
2009-11-23 23:49:18 +0000

    Added WaveServerContainer

---
+++ wave/org.eclipse.ecf.wave/src/org/eclipse/ecf/wave/container/WaveServerContainer.java
@@ -0,0 +1,37 @@
+package org.eclipse.ecf.wave.container;
+
+import org.eclipse.ecf.core.AbstractContainer;
+import org.eclipse.ecf.core.ContainerConnectException;
+import org.eclipse.ecf.core.identity.ID;
+import org.eclipse.ecf.core.identity.Namespace;
+import org.eclipse.ecf.core.security.IConnectContext;
+
+public class WaveServerContainer extends AbstractContainer {
+
+	public void connect(ID targetID, IConnectContext connectContext)
+			throws ContainerConnectException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void disconnect() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Namespace getConnectNamespace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ID getConnectedID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ID getID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
