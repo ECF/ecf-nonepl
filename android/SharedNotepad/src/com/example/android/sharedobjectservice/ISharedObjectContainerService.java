da0d518f6ceeddd33e1a681a3edf6ab67cb8df37
slewis <slewis>
2010-02-08 18:33:36 +0000

    Added shared object service classes

---
+++ android/SharedNotepad/src/com/example/android/sharedobjectservice/ISharedObjectContainerService.java
@@ -0,0 +1,9 @@
+package com.example.android.sharedobjectservice;
+
+import org.eclipse.ecf.core.ContainerCreateException;
+import org.eclipse.ecf.core.sharedobject.ISharedObjectContainer;
+
+public interface ISharedObjectContainerService {
+
+	public ISharedObjectContainer createClientContainer() throws ContainerCreateException;
+}
