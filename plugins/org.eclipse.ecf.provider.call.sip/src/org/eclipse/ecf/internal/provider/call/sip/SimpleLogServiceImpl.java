fd971165251865dc1158a3569616afa46ba6508b
harshana05 <harshana05>
2010-04-02 08:30:07 +0000

    Update

---
+++ plugins/org.eclipse.ecf.provider.call.sip/src/org/eclipse/ecf/internal/provider/call/sip/SimpleLogServiceImpl.java
@@ -0,0 +1,9 @@
+package org.eclipse.ecf.internal.provider.call.sip;
+
+public class SimpleLogServiceImpl implements SimpleLogService {
+
+	public void log(String message) {
+		System.out.println(message);
+	}
+	
+}
