1d1c4f9edee583e15e1449417c26900240a6fc37
wim.jongman <wim.jongman>
2010-04-20 07:54:08 +0000

    Comments, no big deal.

--- newsreader/bundles/org.eclipse.ecf.salvo.ui.navigator/src/org/eclipse/ecf/salvo/ui/navigator/internal/views/GroupView.java
+++ newsreader/bundles/org.eclipse.ecf.salvo.ui.navigator/src/org/eclipse/ecf/salvo/ui/navigator/internal/views/GroupView.java
@@ -28,9 +28,12 @@ import org.eclipse.ecf.salvo.ui.internal.resources.SalvoResourceFactory;
 import org.eclipse.ecf.salvo.ui.tools.SelectionUtil;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.TreePath;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.navigator.CommonNavigator;
+import org.eclipse.ui.navigator.CommonViewer;
 
 public class GroupView extends CommonNavigator implements IStoreEventListener {
 	public static final String ID = "org.eclipse.ui.salvo.application.view";
@@ -68,9 +71,7 @@ public class GroupView extends CommonNavigator implements IStoreEventListener {
 			public void run() {
 				if (event.getEventObject() instanceof INewsgroup
 						|| event.getEventObject() instanceof IServer) {
-					Debug
-							.log(this.getClass(), "Event: "
-									+ event.getEventType());
+					Debug.log(this.getClass(), "Event: " + event.getEventType());
 					TreePath[] elements = getCommonViewer()
 							.getExpandedTreePaths();
 					getCommonViewer().getTree().setRedraw(false);
@@ -81,4 +82,20 @@ public class GroupView extends CommonNavigator implements IStoreEventListener {
 			}
 		});
 	}
+
+//	/**
+//	 * Constructs and returns an instance of {@link CommonViewer}. The ID of the
+//	 * Eclipse view part will be used to create the viewer.
+//	 * 
+//	 * Override this method if you want a subclass of the CommonViewer
+//	 * 
+//	 * @param aParent
+//	 *            A composite parent to contain the CommonViewer
+//	 * @return An instance of CommonViewer
+//	 * @since 3.4
+//	 */
+//	protected CommonViewer createCommonViewerObject(Composite aParent) {
+//		return new CommonViewer(getViewSite().getId(), aParent, SWT.CHECK
+//				| SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+//	}
 }
