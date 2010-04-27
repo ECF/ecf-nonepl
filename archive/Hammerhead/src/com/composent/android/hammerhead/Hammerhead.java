4532f58e8a8043956e21f420bfca877452ea2feb
slewis <slewis>
2010-04-27 17:24:18 +0000

    Initial checkin

---
+++ archive/Hammerhead/src/com/composent/android/hammerhead/Hammerhead.java
@@ -0,0 +1,13 @@
+package com.composent.android.hammerhead;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hammerhead extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
