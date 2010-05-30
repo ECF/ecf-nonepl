00b9f68d7a759d2d1d473520c1a2cac1bff2ca77
mayworm <mayworm>
2010-05-30 14:29:51 +0000

    improving the tests

--- tests/org.eclipse.ecf.examples.tests.remoteservice.soap.host.hello/src/org/eclipse/ecf/examples/tests/remoteservice/soap/host/hello/HelloTest.java
+++ tests/org.eclipse.ecf.examples.tests.remoteservice.soap.host.hello/src/org/eclipse/ecf/examples/tests/remoteservice/soap/host/hello/HelloTest.java
@@ -17,9 +17,12 @@ import org.eclipse.ecf.core.ContainerFactory;
 import org.eclipse.ecf.core.IContainer;
 import org.eclipse.ecf.core.identity.ID;
 import org.eclipse.ecf.core.identity.IDFactory;
+import org.eclipse.ecf.core.util.ECFException;
 import org.eclipse.ecf.examples.remoteservices.hello.IHello;
+import org.eclipse.ecf.remoteservice.IRemoteService;
 import org.eclipse.ecf.remoteservice.IRemoteServiceContainer;
 import org.eclipse.ecf.remoteservice.IRemoteServiceContainerAdapter;
+import org.eclipse.ecf.remoteservice.IRemoteServiceReference;
 import org.eclipse.ecf.remoteservice.RemoteServiceContainer;
 import org.eclipse.ecf.remoteservice.soap.host.ISoapServerContainerAdapter;
 import org.eclipse.ecf.tests.httpservice.AbstractHttpServiceTest;
@@ -51,7 +54,7 @@ public class HelloTest extends AbstractHttpServiceTest {
 						.getAdapter(IRemoteServiceContainerAdapter.class));
 	}
  	
- 	public void testWSService(){
+ 	public void testWSServiceUsingContainerAdapter(){
  		try {
  	 		startHttpService();
  	 	
@@ -59,7 +62,7 @@ public class HelloTest extends AbstractHttpServiceTest {
 
  	 		IRemoteServiceContainer rsContainer = createRemoteServiceContainer(container);
  	 		ID targetID = IDFactory.getDefault().createID(container.getConnectNamespace(),	HOST_CONTAINER_ENDPOINT_ID);
- 	 		soapContainerAdapter.deployRemoteServiceAsWebService(IHello.class.getName(), "*", rsContainer, targetID);
+ 	 		soapContainerAdapter.deployRemoteServiceAsWebService(IHello.class.getName(), "*", rsContainer.getContainerAdapter(), targetID);
  	 		
 			String endpoint = "http://localhost:8089/services/org.eclipse.ecf.examples.remoteservices.hello.IHello";
 
@@ -69,9 +72,50 @@ public class HelloTest extends AbstractHttpServiceTest {
 			call.setTargetEndpointAddress(new java.net.URL(endpoint));
 			call.setOperationName(new QName("hello"));
 
-			call.invoke(new Object[] { "A hello from the Axis client test" });
+			call.invoke(new Object[] { "A hello from the Axis client test using container adapter" });
 			
 			soapContainerAdapter.undeployRemoteServiceAsWebService(IHello.class.getName(), rsContainer, targetID);
+			
+			stopHttpService();
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+ 	}
+
+ 	public void testWSService(){
+ 		try {
+ 	 		startHttpService();
+ 	 	
+			ISoapServerContainerAdapter soapContainerAdapter =  (ISoapServerContainerAdapter) ContainerFactory.getDefault().createContainer().getAdapter(ISoapServerContainerAdapter.class);
+
+ 	 		IRemoteServiceContainer rsContainer = createRemoteServiceContainer(container);
+ 	 		ID targetID = IDFactory.getDefault().createID(container.getConnectNamespace(),	HOST_CONTAINER_ENDPOINT_ID);
+ 	 		
+			// Lookup IRemoteServiceReference
+			IRemoteServiceReference[] serviceReferences = rsContainer.getContainerAdapter().getRemoteServiceReferences(targetID, IHello.class.getName(), null);
+
+			// Get remote service for reference
+			if(serviceReferences == null ||serviceReferences.length == 0)
+				throw new ECFException("The remote reference is not available : "+IHello.class.getName());
+			
+			IRemoteServiceReference reference = serviceReferences[0];			
+			IRemoteService remoteService = rsContainer.getContainerAdapter().getRemoteService(reference);
+
+ 	 		
+ 	 		soapContainerAdapter.deployRemoteServiceAsWebService("hello", "*", remoteService);
+ 	 		
+			String endpoint = "http://localhost:8089/services/hello";
+
+			Service service = new Service();
+			Call call = (Call) service.createCall();
+
+			call.setTargetEndpointAddress(new java.net.URL(endpoint));
+			call.setOperationName(new QName("hello"));
+
+			call.invoke(new Object[] { "A hello from the Axis client test IRemoteService" });
+			
+			soapContainerAdapter.undeployRemoteServiceAsWebService("hello", remoteService);
 			
 			stopHttpService();
 
