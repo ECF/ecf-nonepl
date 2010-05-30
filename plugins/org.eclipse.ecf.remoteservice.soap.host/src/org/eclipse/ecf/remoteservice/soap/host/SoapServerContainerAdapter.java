2e5575d4b4b919c053596171d8834e3714761e45
mayworm <mayworm>
2010-05-30 14:29:05 +0000

    adding more stuff on the API

--- plugins/org.eclipse.ecf.remoteservice.soap.host/src/org/eclipse/ecf/remoteservice/soap/host/SoapServerContainerAdapter.java
+++ plugins/org.eclipse.ecf.remoteservice.soap.host/src/org/eclipse/ecf/remoteservice/soap/host/SoapServerContainerAdapter.java
@@ -15,17 +15,17 @@ import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.IAdapterManager;
 import org.eclipse.ecf.core.identity.ID;
 import org.eclipse.ecf.core.identity.IDCreateException;
-import org.eclipse.ecf.core.identity.IDFactory;
 import org.eclipse.ecf.core.util.ECFException;
 import org.eclipse.ecf.internal.remoteservice.soap.host.Activator;
 import org.eclipse.ecf.internal.remoteservice.soap.host.SOAPServiceTracker;
 import org.eclipse.ecf.remoteservice.IRemoteService;
 import org.eclipse.ecf.remoteservice.IRemoteServiceContainer;
+import org.eclipse.ecf.remoteservice.IRemoteServiceContainerAdapter;
 import org.eclipse.ecf.remoteservice.IRemoteServiceReference;
 import org.osgi.framework.InvalidSyntaxException;
 
 /**
- * Create a soap container which is responsible to locate
+ * Create a soap container adapter which is responsible to publish remote service as WS
  * 
  * @since 3.4
  * 
@@ -50,29 +50,29 @@ public class SoapServerContainerAdapter implements ISoapServerContainerAdapter{
 	 *            . The exposed methods ex.: *. Must not be <code>null</code>.
 	 * @throws ECFException
 	 */
-	public void deployRemoteServiceAsWebService(String clazz, String allowedMethod, IRemoteServiceContainer remoteServiceContainer, ID targetID) throws ECFException {
+	public void deployRemoteServiceAsWebService(String clazz, String allowedMethod, IRemoteServiceContainerAdapter remoteServiceContainerAdapter, ID targetID) throws ECFException {
 		try {
 			Assert.isNotNull(clazz);
 			Assert.isNotNull(allowedMethod);
-
+			Assert.isNotNull(remoteServiceContainerAdapter);
+			Assert.isNotNull(targetID);
+			
 			// Lookup IRemoteServiceReference
-			IRemoteServiceReference[] serviceReferences = remoteServiceContainer.getContainerAdapter()
-					.getRemoteServiceReferences(IDFactory.getDefault()
-							.createID(remoteServiceContainer.getContainer().getConnectNamespace(),
-									targetID.toExternalForm()), clazz, null);
+			IRemoteServiceReference[] serviceReferences = remoteServiceContainerAdapter.getRemoteServiceReferences(targetID, clazz, null);
 
 			// Get remote service for reference
-
 			if(serviceReferences == null ||serviceReferences.length == 0)
 				throw new ECFException("The remote reference is not available : "+clazz);
+			
 			IRemoteServiceReference reference = serviceReferences[0];			
-			IRemoteService remoteService = remoteServiceContainer.getContainerAdapter()
-					.getRemoteService(reference);
+			IRemoteService remoteService = remoteServiceContainerAdapter.getRemoteService(reference);
 
 			// Create the soap service on axis
 			SOAPRemoteService soapService = new SOAPRemoteService(
 					serviceTracker.getSoapServlet().getEngine(), clazz,
 					remoteService.getProxy(), allowedMethod);
+
+			//deploy the service
 			soapService.deployService();
 
 		} catch (AxisFault e) {
@@ -80,6 +80,28 @@ public class SoapServerContainerAdapter implements ISoapServerContainerAdapter{
 		} catch (IDCreateException e) {
 			throw new ECFException("",e); //$NON-NLS-1$
 		} catch (InvalidSyntaxException e) {
+			throw new ECFException("",e); //$NON-NLS-1$
+		}
+
+	}
+
+	public void deployRemoteServiceAsWebService(String serviceName, String allowedMethod, IRemoteService remoteService) throws ECFException {
+		try {
+			Assert.isNotNull(serviceName);
+			Assert.isNotNull(allowedMethod);
+			Assert.isNotNull(remoteService);
+
+			// Create the soap service on axis
+			SOAPRemoteService soapService = new SOAPRemoteService(
+					serviceTracker.getSoapServlet().getEngine(), serviceName,
+					remoteService.getProxy(), allowedMethod);
+
+			//deploy the service
+			soapService.deployService();
+
+		} catch (AxisFault e) {
+			throw new ECFException("", e); //$NON-NLS-1$
+		} catch (IDCreateException e) {
 			throw new ECFException("",e); //$NON-NLS-1$
 		}
 
@@ -120,7 +142,24 @@ public class SoapServerContainerAdapter implements ISoapServerContainerAdapter{
 		}
 
 	}
-	
+
+	public void undeployRemoteServiceAsWebService(String serviceName, IRemoteService remoteService) throws ECFException {
+		try {
+
+			// Create the soap service on axis
+			SOAPRemoteService soapService = new SOAPRemoteService(
+					serviceTracker.getSoapServlet().getEngine(), serviceName,
+					remoteService.getProxy());
+			soapService.undeployService();
+
+		} catch (AxisFault e) {
+			throw new ECFException("", e); //$NON-NLS-1$
+		} catch (IDCreateException e) {
+			throw new ECFException("", e); //$NON-NLS-1$
+		}
+
+	}
+
 	public Object getAdapter(Class adapter) {
 		if (adapter == null)
 			return null;
