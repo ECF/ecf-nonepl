5c2dc6ed9e8fc228e23eff05c7b7d5a8be57b338
wim.jongman <wim.jongman>
2010-05-30 21:02:48 +0000

    Added some tweets

--- newsreader/bundles/org.eclipse.ecf.services.quotes.eclipsetwitter/src/org/eclipse/ecf/services/quotes/eclipsetwitter/QuoteServiceImpl.java
+++ newsreader/bundles/org.eclipse.ecf.services.quotes.eclipsetwitter/src/org/eclipse/ecf/services/quotes/eclipsetwitter/QuoteServiceImpl.java
@@ -6,12 +6,31 @@ import org.eclipse.ecf.services.quotes.QuoteService;
 
 public class QuoteServiceImpl implements QuoteService {
 	final static String NL = System.getenv("line.separator");
-	
-	public static final String[] QUOTES = new String[] { "2+2 = 5 for extremely large values of 2.",
-			"The net knows all! " +
-			NL + "Jeff McAffer - EclipseSource",
+
+	public static final String[] QUOTES = new String[] {
+			"The net knows all! " + NL + "Jeff McAffer - EclipseSource",
 			"is building, testing, building (but with hand crafted bits)",
-			NL + "Paul Webster - IBM" };
+			NL + "Paul Webster",
+			"is sitting still",
+			NL + "Paul Webster",
+			"Taking a shower. Need a couple of good ideas. ;-)",
+			NL + "Boris Bokowski",
+			"Pressure makes diamonds",
+			NL + "Chris Anyszcyk",
+			"In vacation. May the internet survive without me",
+			NL + "Lars Vogel",
+			"BONG BONG BONG BONG",
+			NL + "Big Ben Clock",
+			"wondering if I will ever again give a talk that I didn't prepare up to the minute...",
+			NL + "Peter Friese",
+			"I am all for convention over configuration, the only question is: Whose convention?",
+			NL + "Wim Jongman",
+			"birdstrike today - pigeon into windscreen at 80km/h " + NL
+					+ "- quite a solid thunk " + NL
+					+ "- some dusty smears - alfa 156 2.0l wins)",
+			NL + "Oison Hurley",
+			"learning an important lesson: sometimes the crowd is not that wise",
+			NL + "Ian Skerrett", };
 
 	private Random random;
 
