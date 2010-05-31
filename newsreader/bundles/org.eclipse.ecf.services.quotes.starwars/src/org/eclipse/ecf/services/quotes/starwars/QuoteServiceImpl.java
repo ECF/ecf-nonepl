26de7381c41814a4bd60ce9d2b4ce973c1a1adbd
wim.jongman <wim.jongman>
2010-05-31 15:43:43 +0000

    minor changes

--- newsreader/bundles/org.eclipse.ecf.services.quotes.starwars/src/org/eclipse/ecf/services/quotes/starwars/QuoteServiceImpl.java
+++ newsreader/bundles/org.eclipse.ecf.services.quotes.starwars/src/org/eclipse/ecf/services/quotes/starwars/QuoteServiceImpl.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ *  Copyright (c) 2009 Remain Software, Industrial-TSI & Weltevree Beheer
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *     Wim Jongman - initial API and implementation
+ *
+ *
+ *******************************************************************************/
 package org.eclipse.ecf.services.quotes.starwars;
 
 import java.util.Random;
@@ -45,7 +58,6 @@ public class QuoteServiceImpl implements QuoteService {
 	public String getServiceName() {
 		return "Star Wars Quotes";
 	}
-	
 
 	public String[] getAllQuotes() {
 		return QUOTES;
--- newsreader/bundles/org.eclipse.ecf.services.quotes/src/org/eclipse/ecf/services/quotes/QuoteService.java
+++ newsreader/bundles/org.eclipse.ecf.services.quotes/src/org/eclipse/ecf/services/quotes/QuoteService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2009 Weltevree Beheer BV, Nederland
+ *  Copyright (c) 2009 Remain Software, Industrial-TSI en Weltevree Beheer
  *
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
