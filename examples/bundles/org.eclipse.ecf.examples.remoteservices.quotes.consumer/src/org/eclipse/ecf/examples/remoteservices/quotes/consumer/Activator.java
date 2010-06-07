2368eeed164c5fe5f7398eec6d4b650858ccf677
wim.jongman <wim.jongman>
2010-06-07 09:13:06 +0000

    *** empty log message ***

--- examples/bundles/org.eclipse.ecf.examples.remoteservices.quotes.consumer/src/org/eclipse/ecf/examples/remoteservices/quotes/consumer/Activator.java
+++ examples/bundles/org.eclipse.ecf.examples.remoteservices.quotes.consumer/src/org/eclipse/ecf/examples/remoteservices/quotes/consumer/Activator.java
@@ -1,11 +1,7 @@
 package org.eclipse.ecf.examples.remoteservices.quotes.consumer;
 
-import org.eclipse.ecf.services.quotes.QuoteService;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceEvent;
-import org.osgi.framework.ServiceListener;
-import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
@@ -24,23 +20,6 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
-
-		bundleContext.addServiceListener(new ServiceListener() {
-
-			@Override
-			public void serviceChanged(ServiceEvent event) {
-
-				if (event.getType() == ServiceEvent.REGISTERED) {
-					QuoteService service = (QuoteService) context
-							.getService(event.getServiceReference());
-
-					System.out.print("\n\n\nA new Quote Service: ");
-					System.out.println(service.getServiceDescription());
-					System.out.print("\t" + service.getRandomQuote() + "\n\n");
-
-				}
-			}
-		}, "(objectclass=" + QuoteService.class.getName() + ")");
 
 	}
 
