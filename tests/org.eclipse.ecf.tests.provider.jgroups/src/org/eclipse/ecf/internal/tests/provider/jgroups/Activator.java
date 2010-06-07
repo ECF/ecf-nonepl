5cfae31bae7a25248205fcd3b6abda1cd970a2a5
phperret <phperret>
2010-06-07 15:42:39 +0000

    added JGroups config test

--- tests/org.eclipse.ecf.tests.provider.jgroups/src/org/eclipse/ecf/internal/tests/provider/jgroups/Activator.java
+++ tests/org.eclipse.ecf.tests.provider.jgroups/src/org/eclipse/ecf/internal/tests/provider/jgroups/Activator.java
@@ -1,12 +1,33 @@
 package org.eclipse.ecf.internal.tests.provider.jgroups;
 
+import java.net.URL;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * The activator class controls the plug-in life cycle
  */
 public class Activator extends Plugin {
+
+	private BundleContext context;
+
+	private ServiceTracker extensionRegistryTracker = null;
+
+	private static final String STACK_CONFIG_ID_ATTRIBUTE = "id";
+
+	private static final String STACK_CONFIG_EPOINT = Activator.PLUGIN_ID
+	+ ".stackConfig";
+
+	public static final String STACK_CONFIG_ID = Activator.PLUGIN_ID
+	+ ".default";
+
+	private static final String STACK_CONFIG_FILE_ATTRIBUTE = "configFile";
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.ecf.tests.provider.jgroups";
@@ -26,6 +47,7 @@ public class Activator extends Plugin {
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
+		this.context = context;
 		plugin = this;
 	}
 
@@ -45,6 +67,61 @@ public class Activator extends Plugin {
 	 */
 	public static Activator getDefault() {
 		return plugin;
+	}
+
+	public URL getConfigURLForStackID(String stackID) {
+		if (stackID == null || context == null)
+			return null;
+		final IConfigurationElement configElement = findConfigurationElementForStackID(stackID);
+		if (configElement == null)
+			return null;
+		final String bundleSymbolicName = configElement.getContributor()
+				.getName();
+		final String stackConfigFile = configElement
+				.getAttribute(STACK_CONFIG_FILE_ATTRIBUTE);
+		if (stackConfigFile == null || stackConfigFile.equals(""))
+			return null;
+		final Bundle[] bundles = context.getBundles();
+		for (int i = 0; i < bundles.length; i++) {
+			if (bundles[i].getSymbolicName().equals(bundleSymbolicName))
+				return bundles[i].getResource(stackConfigFile);
+		}
+		return null;
+	}
+
+	/**
+	 * @param stackID
+	 * @return
+	 */
+	private IConfigurationElement findConfigurationElementForStackID(
+			String stackID) {
+		final IExtensionRegistry extensionRegistry = Activator.getDefault()
+				.getExtensionRegistry();
+		if (extensionRegistry == null)
+			return null;
+		final IExtensionPoint extensionPoint = extensionRegistry
+				.getExtensionPoint(STACK_CONFIG_EPOINT);
+		if (extensionPoint == null) {
+			return null;
+		}
+		final IConfigurationElement configurationElements[] = extensionPoint
+				.getConfigurationElements();
+		for (int i = 0; i < configurationElements.length; i++) {
+			final String idAttribute = configurationElements[i]
+					.getAttribute(STACK_CONFIG_ID_ATTRIBUTE);
+			if (idAttribute != null && idAttribute.equals(stackID))
+				return configurationElements[i];
+		}
+		return null;
+	}
+
+	private IExtensionRegistry getExtensionRegistry() {
+		if (extensionRegistryTracker == null) {
+			extensionRegistryTracker = new ServiceTracker(this.context,
+					IExtensionRegistry.class.getName(), null);
+			extensionRegistryTracker.open();
+		}
+		return (IExtensionRegistry) extensionRegistryTracker.getService();
 	}
 
 }
---
+++ tests/org.eclipse.ecf.tests.provider.jgroups/src/org/eclipse/ecf/tests/provider/jgroups/config/JGroupsConfigTest.java
@@ -0,0 +1,84 @@
+package org.eclipse.ecf.tests.provider.jgroups.config;
+
+import java.net.URI;
+import java.net.URL;
+
+import org.eclipse.ecf.internal.tests.provider.jgroups.Activator;
+import org.eclipse.ecf.internal.tests.provider.jgroups.JGroups;
+import org.eclipse.ecf.internal.tests.provider.jgroups.JGroupsContainerTest;
+import org.eclipse.ecf.provider.jgroups.identity.JGroupsID;
+import org.eclipse.ecf.provider.jgroups.identity.JGroupsNamespace;
+import org.jgroups.JChannelFactory;
+
+/**
+ * 
+ * @author pierre
+ * 
+ * 
+ */
+public class JGroupsConfigTest extends JGroupsContainerTest {
+
+	protected static final String JGROUPS_UDP_MCAST_PORT_PROPNAME = "jgroups.udp.mcast_port";
+	protected static final String JGROUPS_UDP_MCAST_ADDR_PROPNAME = "jgroups.udp.mcast_addr";
+
+	private final static String stackConfigID = "org.eclipse.ecf.provider.jgroups.default";
+
+	/**
+	 * stackNamesAvailable lists all 'stacks' contained in file referenced by
+	 * stackConfigID (see extension in o.e.e.jgroups plugin)
+	 * 'bas_config' is NOT present so an error is detected.
+	 */
+	private final static String[] stackNamesAvailable = { "udp", "udp-sync",
+			"tcp", "tcp-sync", "tcp-nio", "tcp-nio-sync", "tunnel",
+			"encrypt_entire_message", "encrypt", "bad_config" };
+
+	protected String oldHost = null;
+	protected int oldPort = -1;
+
+	private String getServerIdentity(int index) {
+		String uri = JGroups.URI_CONFIG_NAME;
+		return uri + "?stackName=" + stackNamesAvailable[index]
+				+ "&stackConfigID=" + stackConfigID;
+	}
+
+	@SuppressWarnings("restriction")
+	private boolean testUri(int index) {
+		URI uri = null;
+		try {
+			uri = new URI(getServerIdentity(index));
+		} catch (Exception e) {
+			assertFalse("bad jgroups uri: " + e.getMessage(), true);
+		}
+		JGroupsID targetID = new JGroupsID(new JGroupsNamespace(), uri);
+		final JChannelFactory factory = new JChannelFactory();
+		final String stackConfigID = targetID.getStackConfigID();
+		URL stackConfigURL = null;
+		stackConfigURL = Activator.getDefault().getConfigURLForStackID(
+				stackConfigID);
+
+		try {
+			factory.setMultiplexerConfig(stackConfigURL);
+			factory.createMultiplexerChannel(targetID.getStackName(),
+					targetID.getName());
+
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+
+	public void testAllGoodStacks() {
+		for (int i = 0; i < stackNamesAvailable.length-1; i++) {
+			assertTrue(testUri(i));
+		}
+	}
+	
+	public void testBadStacks(){
+		assertFalse(testUri(stackNamesAvailable.length));
+	}
+
+	@Override
+	public void testConnectClient() throws Exception {
+	}
+
+}
