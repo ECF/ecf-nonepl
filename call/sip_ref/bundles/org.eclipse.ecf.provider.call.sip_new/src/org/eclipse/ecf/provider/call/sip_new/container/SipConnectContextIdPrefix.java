fb971f016ba4a7b562812f43fad01cbfd0294d5b
mkuppe <mkuppe>
2010-06-10 12:54:26 +0000

    This commit was manufactured by cvs2svn to create tag 'Root_Release_3_3'.

--- call/sip_ref/bundles/org.eclipse.ecf.provider.call.sip_new/src/org/eclipse/ecf/provider/call/sip_new/container/SipConnectContextIdPrefix.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.ecf.provider.call.sip_new.container;
-
-public interface SipConnectContextIdPrefix {
-	
-	public static final String INITIATOR_NAME_PREFIX="InitiatorName";
-	
-	public static final String PROXY_SERVER_PREFIX="SipProxy";
-	
-}
